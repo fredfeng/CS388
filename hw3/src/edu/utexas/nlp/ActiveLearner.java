c7128f093a303ea9719b5da59bea81d6a431559b
Yu Feng <fengyu8299@gmail.com>
2015-04-06 16:18:16 -0500

    sort on each iteration

--- hw3/src/edu/utexas/nlp/ActiveLearner.java
+++ hw3/src/edu/utexas/nlp/ActiveLearner.java
@@ -143,18 +143,17 @@ class ActiveLearner {
 
 		parser = LexicalizedParser.trainFromTreebank(initBank, op);
 
-		if (sel == Selection.RAN) {
-			sortRandom(goldTrees);
-		} else if (sel == Selection.LEN) {
-			sortLength(goldTrees);
-		} else if (sel == Selection.PROB) {
-			sortProb(goldTrees);
-		} else {
-			assert sel == Selection.TREE;
-			sortEntropy(goldTrees);
-		}
-
 		while (iteration > 0) {
+			if (sel == Selection.RAN) {
+				sortRandom(goldTrees);
+			} else if (sel == Selection.LEN) {
+				sortLength(goldTrees);
+			} else if (sel == Selection.PROB) {
+				sortProb(goldTrees);
+			} else {
+				assert sel == Selection.TREE;
+				sortEntropy(goldTrees);
+			}
 			List<Tree> selList = topN(goldTrees, wordsPerIter);
 			List<Tree> copy = new LinkedList<Tree>(selList);
 			goldTrees.removeAll(copy);
@@ -163,9 +162,6 @@ class ActiveLearner {
 			for (Tree init : initTrees) {
 				sb.append(init.pennString());
 			}
-			// for (Tree ran : copy) {
-			// sb.append(ran.pennString());
-			// }
 			// dump to file.
 			try (PrintStream out = new PrintStream(
 					new FileOutputStream(buffLoc))) {
@@ -197,74 +193,6 @@ class ActiveLearner {
         return Sentence.toCoreLabelList(s);
 	}
 
-  /**
-   * demoDP demonstrates turning a file into tokens and then parse
-   * trees.  Note that the trees are printed by calling pennPrint on
-   * the Tree object.  It is also possible to pass a PrintWriter to
-   * pennPrint if you want to capture the output.
-   * This code will work with any supported language.
-   */
-  public static void demoDP(LexicalizedParser lp, String filename) {
-    // This option shows loading, sentence-segmenting and tokenizing
-    // a file using DocumentPreprocessor.
-    TreebankLanguagePack tlp = lp.treebankLanguagePack(); // a PennTreebankLanguagePack for English
-    GrammaticalStructureFactory gsf = null;
-    if (tlp.supportsGrammaticalStructures()) {
-      gsf = tlp.grammaticalStructureFactory();
-    }
-    // You could also create a tokenizer here (as below) and pass it
-    // to DocumentPreprocessor
-    for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
-      Tree parse = lp.apply(sentence);
-      parse.pennPrint();
-      System.out.println();
-
-      if (gsf != null) {
-        GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-        Collection tdl = gs.typedDependenciesCCprocessed();
-        System.out.println(tdl);
-        System.out.println();
-      }
-    }
-  }
-
-  /**
-   * demoAPI demonstrates other ways of calling the parser with
-   * already tokenized text, or in some cases, raw text that needs to
-   * be tokenized as a single sentence.  Output is handled with a
-   * TreePrint object.  Note that the options used when creating the
-   * TreePrint can determine what results to print out.  Once again,
-   * one can capture the output by passing a PrintWriter to
-   * TreePrint.printTree. This code is for English.
-   */
-  public static void demoAPI(LexicalizedParser lp) {
-    // This option shows parsing a list of correctly tokenized words
-    String[] sent = { "This", "is", "an", "easy", "sentence", "." };
-    List<CoreLabel> rawWords = Sentence.toCoreLabelList(sent);
-    Tree parse = lp.apply(rawWords);
-    parse.pennPrint();
-    System.out.println();
-
-    // This option shows loading and using an explicit tokenizer
-    String sent2 = "This is another sentence.";
-    TokenizerFactory<CoreLabel> tokenizerFactory =
-        PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
-    Tokenizer<CoreLabel> tok =
-        tokenizerFactory.getTokenizer(new StringReader(sent2));
-    List<CoreLabel> rawWords2 = tok.tokenize();
-    parse = lp.apply(rawWords2);
-
-    TreebankLanguagePack tlp = lp.treebankLanguagePack(); // PennTreebankLanguagePack for English
-    GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-    GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-    List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
-    System.out.println(tdl);
-    System.out.println();
-
-    // You can also use a TreePrint object to print trees and dependencies
-    TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
-    tp.printTree(parse);
-  }
 
 	private static Treebank makeTreebank(String treebankPath, Options op,
 			FileFilter filt) {
